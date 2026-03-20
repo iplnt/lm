c7cdfd155c3b36c39e829299be36219c299d74a5
IP <interplanetpupiales@gmail.com>
2026-03-19 22:41:11 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-// Este archivo permite que Chrome considere la web como una Aplicación Real
 self.addEventListener('fetch', function(event) {
-  // No hace falta agregar lógica aquí, su sola presencia activa el modo PWA
 });
